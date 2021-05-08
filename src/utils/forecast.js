3460057f270bad507e651d12eca1640b2618de85
SHERRYjain <fbsherry6@gmail.com>
2021-05-08 12:59:48 +0530

    Forcast text changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forcast = (lat,long, callback) => {
             callback('Unable to find location',undefined);
         }
         else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Humidity level is ${body.current.humidity}`)
         }
     });
 }
